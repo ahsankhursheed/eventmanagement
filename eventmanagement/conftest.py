b1a50c0f62cfb9a326c6cdbbe4afe9111d54ea68
Ahsan Khursheed <ahsan.khursheed77@gmail.com>
2024-08-10 03:25:34 +0500

    Setup Django project using cookie cutter

---
+++ eventmanagement/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from eventmanagement.users.models import User
+from eventmanagement.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
